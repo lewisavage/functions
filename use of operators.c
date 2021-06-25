031461e2acfb197f880447ea6b0c2e0a3d3c87fd
lewisavage <85345926+lewisavage@users.noreply.github.com>
2021-06-25 13:41:19 +0300

    Add files via upload

---
+++ use of operators.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+int add,sub,mul,div,rem;
+int a =10 ;
+int b = 5;
+add = a+b;
+printf("result of sum is%d\n",add);
+sub =a-b;
+printf("The result of subtraction is%d\n",sub);
+mul =a*b;
+printf("The result of multiplication is%d\n",mul);
+div =a/b;
+printf("The result of division is%d\n",div);
+rem= a%b;
+printf("The reminder is%d\n",rem);
+
+
+return 0;
+}
